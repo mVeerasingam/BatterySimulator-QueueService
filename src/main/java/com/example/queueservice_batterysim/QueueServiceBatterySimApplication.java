f2ef737ac077eecf41bd3f57efd85cbde6fca785
Mark Veerasingam <markveer70@gmail.com>
2023-11-24 20:48:01 +0000

    first commit

---
+++ src/main/java/com/example/queueservice_batterysim/QueueServiceBatterySimApplication.java
@@ -0,0 +1,13 @@
+package com.example.queueservice_batterysim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueueServiceBatterySimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QueueServiceBatterySimApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/queueservice_batterysim/QueueServiceBatterySimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.queueservice_batterysim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueueServiceBatterySimApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
